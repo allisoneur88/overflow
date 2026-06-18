ff6031e87b0a64296ac4a533be023f0ba1869a1e
allisoneur88 <adganshin@gmail.com>
2026-06-18 10:55:54 +0200

    added get endpoits for questions. added tag controller and a get endpoint for it

--- QuestionService/Controllers/QuestionsController.cs
+++ QuestionService/Controllers/QuestionsController.cs
@@ -10,7 +10,7 @@ namespace QuestionService.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class QuestionsController(QuestionDbContext db): ControllerBase
+public class QuestionsController(QuestionDbContext db) : ControllerBase
 {
     [Authorize]
     [HttpPost]
@@ -18,10 +18,10 @@ public class QuestionsController(QuestionDbContext db): ControllerBase
     {
         var validTags = await db.Tags.Where(x => dto.Tags.Contains(x.Slug)).ToListAsync();
         var missing = dto.Tags.Except(validTags.Select(x => x.Slug)).ToList().ToList();
-        
+
         if (missing.Count != 0)
             return BadRequest($"Invalid tags: {string.Join(", ", missing)}");
-        
+
         var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
         var name = User.FindFirstValue("name");
 
@@ -35,10 +35,37 @@ public class QuestionsController(QuestionDbContext db): ControllerBase
             AskerId = userId,
             AskerDisplayName = name,
         };
-        
+
         db.Questions.Add(question);
         await db.SaveChangesAsync();
-        
+
         return Created($"questions/{question.Id}", question);
     }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Question>>> GetQuestions(string? tag)
+    {
+        var query = db.Questions.AsQueryable();
+
+        if (!string.IsNullOrEmpty(tag))
+        {
+            query = query.Where(x => x.TagSlugs.Contains(tag));
+        }
+
+        return await query.OrderByDescending(x => x.CreatedAt).ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Question>> GetQuestion(string id)
+    {
+        var question = await db.Questions.FindAsync(id);
+        
+        if (question is null) return NotFound();
+        
+        await db.Questions.Where(x => x.Id == id)
+            .ExecuteUpdateAsync(setters => setters.SetProperty(x => x.ViewCount, x => x.ViewCount + 1));
+
+        return question;
+    }
+
 }
