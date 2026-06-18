d6480d0262429bad4341e9ec0b1be270bbe0bae3
allisoneur88 <adganshin@gmail.com>
2026-06-19 00:02:54 +0200

    added search service stub and added typesense to the apphost

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -10,6 +10,13 @@ var postgres = builder.AddPostgres("postgres", port: 5432)
     .WithDataVolume("postgres-data")
     .WithPgAdmin();
 
+var typesense = builder.AddContainer("typesense", "typesense/typesense", "30.2")
+    .WithArgs("--data-dir", "/data", "--api-key", "xyz", "--enable-cors")
+    .WithVolume("typesense-data", "/data")
+    .WithEndpoint(8108, 8108, name: "typesense");
+
+var typesenseContainer = typesense.GetEndpoint("typesense");
+
 var questionDb = postgres.AddDatabase("questionDb");
 
 var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
@@ -17,5 +24,9 @@ var questionService = builder.AddProject<Projects.QuestionService>("question-svc
     .WithReference(questionDb)
     .WaitFor(keycloak)
     .WaitFor(questionDb);
+
+var searchService = builder.AddProject<Projects.SearchService>("search-svc")
+    .WithReference(typesenseContainer)
+    .WaitFor(typesense);
 
 builder.Build().Run();
